11faadc56d9220bc34ed207371356a9afffe457c
unknown <kavinprathaban025@gmail.com>
2020-10-27 17:24:12 +0530

    git initialized, frontend, backend apps created

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
+
+app.listen(5000, console.log('Server is running on port 5000'))
