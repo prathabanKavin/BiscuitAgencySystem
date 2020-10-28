7032640758cfc2d6284bcf3f857072312cfab851
unknown <kavinprathaban025@gmail.com>
2020-10-29 02:43:37 +0530

    modified components and added routing

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config()
+
+//database connection
+connectDB()
 
 const app = express();
 
@@ -6,4 +13,6 @@ app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-app.listen(5000, console.log('Server is running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`))
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Container, Navbar, Nav } from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar bg="primary" variant='dark' expand="lg" collapseOnSelect>
+                <Container>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>Sewwandi Distributors</Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                        <LinkContainer to='/login'>
+                            <Nav.Link><i className='fas fa-user'></i> SignIn </Nav.Link>
+                        </LinkContainer>  
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
