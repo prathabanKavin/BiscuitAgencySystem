3fc3e2cf1766c1fc6923a8a06e0ef87e559482dd
John Doe <johndoe@example.com>
2020-10-28 16:39:02 +0530

    header,footer components added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
-
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+   
 
     </div>
   );
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+
+export default class Header extends Component {
+    render() {
+        return (
+
+            <div className="Header">
+                <nav class="navbar navbar-dark bg-primary">
+                   <div className="nav-main">
+                       <h1>Biscuits</h1>
+                   </div>
+                </nav>
+            </div>
+        );
+    }
+}
