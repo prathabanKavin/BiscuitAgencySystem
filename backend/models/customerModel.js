50b7c02c680a9b4a580d42f086cc878c474f4d0d
Lukshankavinda <60281719+Lukshankavinda@users.noreply.github.com>
2020-10-30 20:26:23 +0530

    customermodel and sample data created

---
+++ backend/models/customerModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const customernSchema = mongoose.Schema({
+    cust_name : {
+        type: String,
+        required: true
+    },
+    cust_nic : {
+        type: String,
+        required: true,
+    },
+    cust_address : {
+        road: {type:String, required: true},
+        city: {type:String, required: true},
+        postalCode: {type:String, required: true}
+    },
+    cust_phone : {
+        type: String,
+        required: true,
+    },
+    cust_email : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    cust_UserName : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password : {
+        type: String,
+        required: true
+    },
+    
+}, {
+    timestamps : true
+})
+
+const Customer = mongoose.model('Customer', customernSchema)
+
+module.exports = Customer
