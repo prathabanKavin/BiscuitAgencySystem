f38b917b7b1407061dfbe660a7a0cde6df396c90
unknown <kavinprathaban025@gmail.com>
2020-11-02 01:16:24 +0530

    products sample data updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Header/>
       <main className='py-3'>
         <Container>
-          <Route path='/' component={HomeScreen}/>
+          <Route path='/' component={HomeScreen} exact/>
+          <Route path='/product/:id' component={ProductScreen}/>
         </Container>
       </main>
       <Footer/>
