be14a8370ed71ddfc3036beaad420147eab84345
Panoja123 <Panobaby@gamil.com>
2020-11-16 22:12:04 +0530

    loading component and alert component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,7 @@
 import React , { useEffect } from 'react'
 import Product from '../components/Product'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import {useDispatch, useSelector} from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import { listProducts } from '../actions/productActions'
@@ -20,9 +22,9 @@ const HomeScreen = () => {
         <>
             <h1>Latest Products</h1>
             {loading ? (
-                <h2>Loading...</h2>
+              <Loader />  
             ) : error ? (
-                <h3>{error}</h3>
+                <Message variant='danger'>{error}</Message>  
             ) : (
                 <Row>
                     {products.map(product => (
