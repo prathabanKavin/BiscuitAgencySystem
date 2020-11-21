bf995a974e2b12385c8fe2edd14fb3d9b6c098f7
prathabanKavin <kavinprathaban025@gmail.com>
2020-11-21 23:14:18 +0530

    cartActions updated

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -4,7 +4,6 @@ import { CART_ADD_ITEM } from '../constants/cartConstants'
 export const addToCart = (id, qty) => async (dispatch, getState) => {
     const { data } = await axios.get(`/api/products/${id}`)
 
-
     dispatch({
         type: CART_ADD_ITEM,
         payload: {
@@ -13,10 +12,9 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
             image: data.image,
             price: data.price,
             countInStock: data.countInStock,
-            cart
+            qty
         }
-
-
     })
+
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,8 +10,7 @@ const reducer = combineReducers({
     cart: cartReducer,
 })
 
-const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse
-    (localStorage.getItem('cartItems')) : []
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
 const initialState = {
     cart: { cartItems: cartItemsFromStorage },
