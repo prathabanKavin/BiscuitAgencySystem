83dcdf7e8a8a5af710a37cc36f6ebd0279e9fe68
Sham <Shamshan>
2020-11-23 10:35:01 +0530

    implement add to cart functionality in cartscreen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,7 +5,7 @@ import {
     PRODUCT_LIST_FAIL,
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
-    PRODUCT_DETAILS_FAIL
+    PRODUCT_DETAILS_FAIL    
 } from '../constants/productConstants'
 
 export const listProducts = () => async (dispatch) => {
--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -14,7 +14,7 @@ const Loader  = () =>
             }}
             >
 
-        <span class='sr-only'>Loading....</span>
+        <span className='sr-only'>Loading....</span>
          </Spinner>
     )
 }
