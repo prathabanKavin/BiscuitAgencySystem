8032dc8ca41072bb1d17363c3a5ba04f079b7fb4
prathabanKavin <kavinprathaban025@gmail.com>
2020-11-29 23:52:49 +0530

    created the productController

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,28 +1,10 @@
 const express = require('express')
-const asyncHandler = require('express-async-handler')
-const router =express.Router()
-const Product = require('../models/productModel')
+const router = express.Router()
+const productController = require('../controllers/productController')
 
-// @desc        Fetch all products
-// @route       GET /api/products
-// @access      Public
-router.get('/', asyncHandler(async(req, res) => {
-    const products= await Product.find({})
-    res.json(products)
-}))
+const { getProducts, getProductById } = productController
 
-// @desc        Fetch single product by id
-// @route       GET /api/products/:id
-// @access      Public
-router.get('/:id', asyncHandler(async(req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if(product){
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('Product Not Found')
-    }
-}))
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
 module.exports = router
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -36,7 +36,7 @@ const CartScreen = ({match ,location,history}) => {
                     <h1>Shopping Cart</h1> 
                     {cartItems.length === 0 ? (
                         <Message>
-                            Your cart is empty<link to='/'>Go Back</link>
+                            Your cart is empty  <Link to='/'> Go Back</Link>
                         </Message>
                     ) : (
                         <ListGroup variant='flush'>
@@ -49,7 +49,7 @@ const CartScreen = ({match ,location,history}) => {
                                         <Col md={3}>
                                            <Link to={`/product/${item.product}`}>{item.name}</Link>
                                         </Col>
-                                        <Col md={2}>${item.price}</Col>
+                                        <Col md={2}>LKR {item.price}</Col>
                                         <Col md={2}>
                                         <Form.Control as='select' value={item.qty} onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}>
                                                     {
@@ -77,7 +77,7 @@ const CartScreen = ({match ,location,history}) => {
                    <ListGroup variant='flush'>
                        <ListGroup.Item>
                            <h2>Subtotal({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items </h2>
-                           ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
+                           LKR {cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
                        </ListGroup.Item>
                        <ListGroup.Item>
                            <Button 
