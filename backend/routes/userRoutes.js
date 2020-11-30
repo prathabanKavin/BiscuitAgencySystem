37b3ff39dc720928ed4f8340412509cb265eaf3b
prathabanKavin <kavinprathaban025@gmail.com>
2020-11-30 23:57:34 +0530

    register endpoint created

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router()
 const userController = require('../controllers/userController')
 const protect = require('../middleware/authMiddleware')
 
-const { authUser, getUserProfile } = userController
+const { authUser, registerUser, getUserProfile } = userController
 
+router.route('/').post(registerUser)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
 
