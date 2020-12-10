4a52b17ba7d4fcfb5151fc22a44e4cb8ced401ed
prathabanKavin <kavinprathaban025@gmail.com>
2020-12-10 23:06:37 +0530

    orderPay reducer and action created

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -22,7 +22,7 @@ const OrderScreen = ({ match }) => {
     }
     useEffect(() => {
         dispatch(getOrderDetails(orderId))
-    }, [])
+    }, [dispatch, orderId])
 
     return loading ? <Loader /> : error ? <Message variant='danger'>{error}
         </Message> : 
