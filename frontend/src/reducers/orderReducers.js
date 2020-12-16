12351c1b12d3d7ef82cf3872de62528af5aaaa92
Isuru <isuru1996617@gmail.com>
2020-12-16 21:52:26 +0530

    resolve the bug :My Order ,Profile list not reset after log out in profile screen

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -11,7 +11,8 @@ import {
     ORDER_PAY_RESET,
     ORDER_LIST_MY_REQUEST,
     ORDER_LIST_MY_SUCCESS,
-    ORDER_LIST_MY_FAIL
+    ORDER_LIST_MY_FAIL,
+    ORDER_LIST_MY_RESET,
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -52,7 +53,8 @@ export const orderDetailsReducer = (state = { loading : true, orderItems: [] , s
             return{
                 loading: false,
                 error: action.payload,
-            }    
+            } 
+        
         default:
             return state       
     }
@@ -97,6 +99,9 @@ export const orderListMyReducer = (state = { orders:[] }, action) => {
                 loading: false,
                 error: action.payload,
             }
+            case ORDER_LIST_MY_RESET:{
+                return{ orders:[] }
+            }   
         default:
             return state       
     }
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -8,6 +8,7 @@ import {
     USER_REGISTER_FAIL,
     USER_DETAILS_REQUEST,
     USER_DETAILS_SUCCESS,
+    USER_DETAILS_RESET,
     USER_DETAILS_FAIL,
     USER_UPDATE_PROFILE_REQUEST,
     USER_UPDATE_PROFILE_SUCCESS,
@@ -52,6 +53,8 @@ export const userDetailsReducer = (state = { user:{} }, action) => {
             return { loading: false, user: action.payload }
         case USER_DETAILS_FAIL:
             return { loading: false, error: action.payload }
+        case USER_DETAILS_RESET:
+            return { user:{} }
         default:
             return state
     }
