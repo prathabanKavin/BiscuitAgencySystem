8679302a518dbc88bad2b5e14169a5c929c6798c
chamika94 <chamikasandaruwan1994@gmail.com>
2020-12-18 21:58:54 +0530

    created UserLists for admin panel

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -6,6 +6,9 @@ import {
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
     USER_REGISTER_FAIL,
+    USER_LIST_REQUEST,
+    USER_LIST_SUCCESS,
+    USER_LIST_FAIL,
     USER_DETAILS_REQUEST,
     USER_DETAILS_SUCCESS,
     USER_DETAILS_RESET,
@@ -67,6 +70,19 @@ export const userUpdateProfileReducer = (state = { }, action) => {
         case USER_UPDATE_PROFILE_SUCCESS:
             return { loading: false, success: true, userInfo: action.payload }
         case USER_UPDATE_PROFILE_FAIL:
+            return { loading: false, error: action.payload }
+        default:
+            return state
+    }
+}
+
+export const userListReducer = (state = { user: [] }, action) => {
+    switch (action.type) {
+        case USER_LIST_REQUEST:
+            return { loading: true }
+        case USER_LIST_SUCCESS:
+            return { loading: false, users: action.payload }
+        case USER_LIST_FAIL:
             return { loading: false, error: action.payload }
         default:
             return state
