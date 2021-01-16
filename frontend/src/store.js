72ea97b9441d1bc696e6a52b1ebca4bb05a86a99
chamika94 <chamikasandaruwan1994@gmail.com>
2021-01-17 00:15:24 +0530

    Created adminUpdateProduct feature

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer, productDetailsReducer ,productDeleteReducer,productCreateReducer, } from './reducers/productReducers'
+import { productListReducer, productDetailsReducer ,productDeleteReducer,productCreateReducer,productUpdateReducer, } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import {
     userLoginReducer,
@@ -24,6 +24,7 @@ const reducer = combineReducers({
     productDetails: productDetailsReducer,
     productDelete: productDeleteReducer,
     productCreate: productCreateReducer,
+    productUpdate: productUpdateReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
