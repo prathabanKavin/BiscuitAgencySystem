c1de8b1f860a3c89bd6ceb0735a2554a260590a8
prathabanKavin <kavinprathaban025@gmail.com>
2021-01-20 01:52:34 +0530

    created frontend imageUpload feature

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -4,35 +4,38 @@ const multer = require('multer')
 const router = express.Router()
 
 const storage = multer.diskStorage({
-    destination(req, file, cb){
+    destination(req, file, cb) {
         cb(null, 'uploads/')
     },
-    filename(req, file, cb){
-        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
-    }
+    filename(req, file, cb) {
+        cb(
+            null,
+            `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+        )
+    },
 })
-
-function checkFileType(file, cb){
+  
+function checkFileType(file, cb) {
     const filetypes = /jpg|jpeg|png/
     const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
     const mimetype = filetypes.test(file.mimetype)
-
-    if(extname && mimetype){
-        return cb(null, true)
+  
+    if (extname && mimetype) {
+      return cb(null, true)
     } else {
-        cb('Images only!')
+      cb('Images only!')
     }
 }
-
+  
 const upload = multer({
     storage,
-    fileFilter: function(req, file, cb){
-        checkFileType(file, cb)
-    }
+    fileFilter: function (req, file, cb) {
+      checkFileType(file, cb)
+    },
 })
-
+  
 router.post('/', upload.single('image'), (req, res) => {
-    res.send(`${req.file.path}`)
+    res.send(`/${req.file.path}`)
 })
 
 module.exports = router
--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
@@ -8,7 +9,6 @@ import FormContainer from '../components/FormContainer'
 import { listProductDetails, updateProduct } from '../actions/productActions'
 import {PRODUCT_UPDATE_RESET} from '../constants/productConstants'
 
-
 const ProductEditScreen = ({ match, history }) => {
     const productId = match.params.id
     const [name, setName] = useState('')
@@ -18,6 +18,7 @@ const ProductEditScreen = ({ match, history }) => {
     const [category, setCategory] = useState('')
     const [countInStock, setCountInStock] = useState(0)
     const [description , setDescription] = useState('')
+    const [uploading , setUploading] = useState(false)
     
     const dispatch = useDispatch()
 
@@ -46,6 +47,26 @@ const ProductEditScreen = ({ match, history }) => {
             }
     }, [dispatch, history, productId, product, successUpdate])
 
+    const uploadFileHandler = async (e) => {
+        const file = e.target.files[0]
+        const formData = new FormData()
+        formData.append('image', file)
+        setUploading(true)
+        try {
+            const config = {
+                headers : {
+                    'Content-Type':'multipart/form-data'
+                }
+            }
+            const { data } = await axios.post('/api/upload', formData, config)
+            setImage(data)
+            setUploading(false)
+        } catch (error) {
+            console.error(error)
+            setUploading(false)
+        }
+    }
+    
     const submitHandler = (e) => {
         e.preventDefault()
         dispatch(
@@ -96,6 +117,8 @@ const ProductEditScreen = ({ match, history }) => {
                         value={image}
                         onChange={(e) => setImage(e.target.value)}
                     ></Form.Control>
+                    <Form.File id='image-file' label='Choose File' custom onChange={uploadFileHandler}></Form.File>
+                    {uploading && <Loader/>}
                 </Form.Group>
                 <Form.Group controlId='brand'>
                 <Form.Label>Brand</Form.Label>
@@ -129,12 +152,9 @@ const ProductEditScreen = ({ match, history }) => {
                         onChange={(e) => setDescription(e.target.value)}
                     ></Form.Control>
                 </Form.Group>
-
-                
                 <Button type='submit' variant='primary'>Update</Button>
             </Form>
             )}
-            
         </FormContainer>
         </>
     )
