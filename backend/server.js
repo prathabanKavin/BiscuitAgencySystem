c70c3b7dca2bf4f36e7d93824de691348a34c732
Isuru <isuru1996617@gmail.com>
2021-02-04 23:31:28 +0530

    create the review endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 const connectDB = require('./config/db')
 const notFound = require('./middleware/notFound')
 const errorHandler = require('./middleware/errorHandler')
@@ -15,6 +16,10 @@ dotenv.config();
 connectDB()
 
 const app = express();
+
+if(process.env.NODE_ENV === 'development'){
+ app.use(morgan('dev'))
+}
 
 //json body parser middleware
 app.use(express.json())
