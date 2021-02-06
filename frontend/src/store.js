2506eb6fd5a52124dfe6ec42c7500b813dac5eaf
Isuru <isuru1996617@gmail.com>
2021-02-06 11:26:45 +0530

    ? error.response.data.message

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer, productDetailsReducer ,productDeleteReducer,productCreateReducer,productUpdateReducer, } from './reducers/productReducers'
+import { productListReducer, productDetailsReducer ,productDeleteReducer,productCreateReducer,productUpdateReducer,productReviewCreateReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import {
     userLoginReducer,
@@ -26,6 +26,7 @@ const reducer = combineReducers({
     productDelete: productDeleteReducer,
     productCreate: productCreateReducer,
     productUpdate: productUpdateReducer,
+    productReviewCreate: productReviewCreateReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
